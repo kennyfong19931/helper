f3fc4b1fe445948b64deb1a02cd94550db914e09
Kenny Fong <kennyfong19931@gmail.com>
2025-01-16 10:33:01 +0800

    update update.bat

--- update.bat
+++ update.bat
@@ -8,10 +8,17 @@ echo ">>> abema"
 call npm run bdh hokan abema
 echo ">>> danime"
 call npm run bdh hokan danime
+echo ">>> prime"
+call npm run bdh hokan prime
+echo ">>> netflix"
+call npm run bdh hokan netflix
+echo ">>> disneyplus"
+call npm run bdh hokan disneyplus
+echo ">>> crunchyroll"
+call npm run bdh hokan crunchyroll
+
 echo ">>> muse_hk"
 call npm run bdh hokan muse_hk
-@REM echo ">>> muse_tw"
-@REM call npm run bdh hokan muse_tw
 echo ">>> ani_one"
 call npm run bdh hokan ani_one
 echo ">>> ani_one_asia"
@@ -20,26 +27,24 @@ echo ">>> tropics"
 call npm run bdh hokan tropics
 echo ">>> viu"
 call npm run bdh hokan viu
-echo ">>> netflix"
-call npm run bdh hokan netflix
+echo ">>> gamer_hk"
+call npm run bdh hokan gamer_hk
 echo ">>> mytv"
 call npm run bdh hokan mytv
-echo ">>> disneyplus"
-call npm run bdh hokan disneyplus
-echo ">>> crunchyroll"
-call npm run bdh hokan crunchyroll
-@REM echo ">>> bilibili"
-@REM call npm run bdh hokan bilibili
 echo ">>> bilibili_hk_mo_tw"
 call npm run bdh hokan bilibili_hk_mo_tw
 echo ">>> bilibili_hk_mo"
 call npm run bdh hokan bilibili_hk_mo
+
+@REM echo ">>> bilibili"
+@REM call npm run bdh hokan bilibili
+
 @REM echo ">>> bilibili_tw"
 @REM call npm run bdh hokan bilibili_tw
 @REM echo ">>> gamer"
 @REM call npm run bdh hokan gamer
-echo ">>> gamer_hk"
-call npm run bdh hokan gamer_hk
+@REM echo ">>> muse_tw"
+@REM call npm run bdh hokan muse_tw
 
 echo ">>> dmhy"
 call npm run bdh hokan dmhy

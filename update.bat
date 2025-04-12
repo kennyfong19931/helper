85be25d17b348ee771a54fe2fde89db23023daf6
Kenny Fong <kennyfong19931@gmail.com>
2025-04-12 14:11:55 +0800

    npx

--- update.bat
+++ update.bat
@@ -1,57 +1,57 @@
 @REM 需要注意續集同ID: abema, netfilx, disneyplus, bilibili, muse_hk, ani_one, ani_one_asia, crunchyroll
 
 echo ">>> nicovideo"
-call npm run bdh hokan nicovideo
+call npx bdh hokan nicovideo
 echo ">>> unext"
-call npm run bdh hokan unext
+call npx bdh hokan unext
 echo ">>> abema"
-call npm run bdh hokan abema
+call npx bdh hokan abema
 echo ">>> danime"
-call npm run bdh hokan danime
+call npx bdh hokan danime
 echo ">>> prime"
-call npm run bdh hokan prime
+call npx bdh hokan prime
 echo ">>> netflix"
-call npm run bdh hokan netflix
+call npx bdh hokan netflix
 echo ">>> disneyplus"
-call npm run bdh hokan disneyplus
+call npx bdh hokan disneyplus
 echo ">>> crunchyroll"
-call npm run bdh hokan crunchyroll
+call npx bdh hokan crunchyroll
 
 echo ">>> muse_hk"
-call npm run bdh hokan muse_hk
+call npx bdh hokan muse_hk
 echo ">>> ani_one"
-call npm run bdh hokan ani_one
+call npx bdh hokan ani_one
 echo ">>> ani_one_asia"
-call npm run bdh hokan ani_one_asia
+call npx bdh hokan ani_one_asia
 echo ">>> tropics"
-call npm run bdh hokan tropics
+call npx bdh hokan tropics
 echo ">>> viu"
-call npm run bdh hokan viu
+call npx bdh hokan viu
 echo ">>> gamer_hk"
-call npm run bdh hokan gamer_hk
+call npx bdh hokan gamer_hk
 echo ">>> mytv"
-call npm run bdh hokan mytv
+call npx bdh hokan mytv
 echo ">>> bilibili_hk_mo_tw"
-call npm run bdh hokan bilibili_hk_mo_tw
+call npx bdh hokan bilibili_hk_mo_tw
 echo ">>> bilibili_hk_mo"
-call npm run bdh hokan bilibili_hk_mo
+call npx bdh hokan bilibili_hk_mo
 
 @REM echo ">>> bilibili"
-@REM call npm run bdh hokan bilibili
+@REM call npx bdh hokan bilibili
 
 @REM echo ">>> bilibili_tw"
-@REM call npm run bdh hokan bilibili_tw
+@REM call npx bdh hokan bilibili_tw
 @REM echo ">>> gamer"
-@REM call npm run bdh hokan gamer
+@REM call npx bdh hokan gamer
 @REM echo ">>> muse_tw"
-@REM call npm run bdh hokan muse_tw
+@REM call npx bdh hokan muse_tw
 
 echo ">>> dmhy"
-call npm run bdh hokan dmhy
+call npx bdh hokan dmhy
 echo ">>> mikan"
-call npm run bdh hokan mikan
+call npx bdh hokan mikan
 echo ">>> bangumi_moe"
-call npm run bdh hokan bangumi_moe
+call npx bdh hokan bangumi_moe
 
 
-@REM call npm run bdh update 202501
+@REM call npx bdh update 202504

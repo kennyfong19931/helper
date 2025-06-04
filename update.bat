b9063b6cb796845562bfc25f093efbd4120aad2b
Kenny Fong <kennyfong19931@gmail.com>
2025-06-04 21:16:56 +0800

    fix typo

--- update.bat
+++ update.bat
@@ -1,4 +1,4 @@
-@REM 需要注意續集同ID: abema, netfilx, disneyplus, bilibili, muse_hk, ani_one, ani_one_asia, crunchyroll
+@REM 需要注意續集同ID: abema, netflix, disneyplus, bilibili, muse_hk, ani_one, ani_one_asia, crunchyroll
 
 echo ">>> nicovideo"
 call npx bdh hokan nicovideo

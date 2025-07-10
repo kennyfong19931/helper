c7687a8a6559403d6280c18955d59d0e1b7db091
Kenny Fong <kennyfong19931@gmail.com>
2025-07-10 16:41:09 +0800

    update update.bat

--- update.bat
+++ update.bat
@@ -46,12 +46,61 @@ call npx bdh hokan bilibili_hk_mo
 @REM echo ">>> muse_tw"
 @REM call npx bdh hokan muse_tw
 
-echo ">>> dmhy"
-call npx bdh hokan dmhy
-echo ">>> mikan"
-call npx bdh hokan mikan
-echo ">>> bangumi_moe"
-call npx bdh hokan bangumi_moe
+@REM echo ">>> dmhy"
+@REM call npx bdh hokan dmhy
+@REM echo ">>> mikan"
+@REM call npx bdh hokan mikan
+@REM echo ">>> bangumi_moe"
+@REM call npx bdh hokan bangumi_moe
+
+
+
+@REM echo ">>> nicovideo"
+@REM call npx bdh cleanup nicovideo
+@REM echo ">>> abema"
+@REM call npx bdh cleanup abema
+@REM echo ">>> unext"
+@REM call npx bdh cleanup unext
+@REM echo ">>> danime"
+@REM call npx bdh cleanup danime
+@REM echo ">>> prime"
+@REM call npx bdh cleanup prime
+@REM echo ">>> netflix"
+@REM call npx bdh cleanup netflix
+@REM echo ">>> disneyplus"
+@REM call npx bdh cleanup disneyplus
+@REM echo ">>> crunchyroll"
+@REM call npx bdh cleanup crunchyroll
+
+@REM echo ">>> muse_hk"
+@REM call npx bdh cleanup muse_hk
+@REM echo ">>> ani_one"
+@REM call npx bdh cleanup ani_one
+@REM echo ">>> ani_one_asia"
+@REM call npx bdh cleanup ani_one_asia
+@REM echo ">>> tropics"
+@REM call npx bdh cleanup tropics
+@REM echo ">>> viu"
+@REM call npx bdh cleanup viu
+@REM echo ">>> gamer_hk"
+@REM call npx bdh cleanup gamer_hk
+@REM echo ">>> mytv"
+@REM call npx bdh cleanup mytv
+@REM echo ">>> bilibili_hk_mo_tw"
+@REM call npx bdh cleanup bilibili_hk_mo_tw
+@REM echo ">>> bilibili_hk_mo"
+@REM call npx bdh cleanup bilibili_hk_mo
+
+@REM echo ">>> bilibili"
+@REM call npx bdh cleanup bilibili
+
+@REM echo ">>> bilibili_tw"
+@REM call npx bdh cleanup bilibili_tw
+@REM echo ">>> gamer"
+@REM call npx bdh cleanup gamer
+@REM echo ">>> muse_tw"
+@REM call npx bdh cleanup muse_tw
+
 
 
 @REM call npx bdh update 202507

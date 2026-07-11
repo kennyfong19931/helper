4b72e58765cb154f4d4db799ea8f76559a685195
Kenny Fong <kennyfong19931@gmail.com>
2026-07-11 22:02:19 +0800

    update update.bat

--- update.bat
+++ update.bat
@@ -1,41 +1,43 @@
 @REM 需要注意續集同ID: abema, netflix, disneyplus, bilibili, muse_hk, ani_one, ani_one_asia, crunchyroll
 @REM git apply --reject --whitespace=fix ../bangumi-data/helper.patch
 
-echo ">>> nicovideo"
-call npx bdh hokan nicovideo
-echo ">>> unext"
-call npx bdh hokan unext
-echo ">>> abema"
-call npx bdh hokan abema
-echo ">>> danime"
-call npx bdh hokan danime
-echo ">>> prime"
-call npx bdh hokan prime
-echo ">>> netflix"
-call npx bdh hokan netflix
-echo ">>> disneyplus"
-call npx bdh hokan disneyplus
-echo ">>> crunchyroll"
-call npx bdh hokan crunchyroll
+@REM echo ">>> nicovideo"
+@REM call npx bdh hokan nicovideo
+@REM echo ">>> unext"
+@REM call npx bdh hokan unext
+@REM echo ">>> abema"
+@REM call npx bdh hokan abema
+@REM echo ">>> danime"
+@REM call npx bdh hokan danime
+@REM echo ">>> prime"
+@REM call npx bdh hokan prime
+@REM echo ">>> netflix"
+@REM call npx bdh hokan netflix
+@REM echo ">>> disneyplus"
+@REM call npx bdh hokan disneyplus
+@REM echo ">>> crunchyroll"
+@REM call npx bdh hokan crunchyroll
 
-echo ">>> muse_hk"
-call npx bdh hokan muse_hk
-echo ">>> ani_one"
-call npx bdh hokan ani_one
-echo ">>> ani_one_asia"
-call npx bdh hokan ani_one_asia
-echo ">>> tropics"
-call npx bdh hokan tropics
-echo ">>> viu"
-call npx bdh hokan viu
-echo ">>> gamer_hk"
-call npx bdh hokan gamer_hk
-echo ">>> mytv"
-call npx bdh hokan mytv
-echo ">>> bilibili_hk_mo_tw"
-call npx bdh hokan bilibili_hk_mo_tw
-echo ">>> bilibili_hk_mo"
-call npx bdh hokan bilibili_hk_mo
+@REM echo ">>> muse_hk"
+@REM call npx bdh hokan muse_hk
+@REM echo ">>> ani_one"
+@REM call npx bdh hokan ani_one
+@REM echo ">>> ani_one_asia"
+@REM call npx bdh hokan ani_one_asia
+@REM echo ">>> tropics"
+@REM call npx bdh hokan tropics
+@REM echo ">>> mighty"
+@REM call npx bdh hokan mighty
+@REM echo ">>> viu"
+@REM call npx bdh hokan viu
+@REM echo ">>> gamer_hk"
+@REM call npx bdh hokan gamer_hk
+@REM echo ">>> mytv"
+@REM call npx bdh hokan mytv
+@REM echo ">>> bilibili_hk_mo_tw"
+@REM call npx bdh hokan bilibili_hk_mo_tw
+@REM echo ">>> bilibili_hk_mo"
+@REM call npx bdh hokan bilibili_hk_mo
 
 @REM echo ">>> bilibili"
 @REM call npx bdh hokan bilibili
@@ -120,4 +122,4 @@ call npx bdh hokan bilibili_hk_mo
 @REM call npx bdh validate tmdbBegin
 
 
-@REM call npx bdh update 202604
+@REM call npx bdh update 202607
